79a69b34c6bc451ab22c7f989f8778ec6218e3ae
Chris Moultrie <cmoultrie@pindropsecurity.com>
2015-07-20 18:34:58 -0400

    URL Endpoint

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+});
+
+router.post('/', function(req, res, next) {
+    console.log(JSON.stringify(req.body, null, 2));
+    res.send('Accepted')
 });
 
 module.exports = router;
